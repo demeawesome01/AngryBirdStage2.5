c4e1666bc521eeaec5087d2959c99340632af5a9
demeawesome01 <demeawesome01@gmail.com>
2020-10-21 19:03:56 -0700

    created box10

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,box10
 var backgroundImg,platform;
 
 function preload() {
@@ -23,7 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    box10 = new Box(100,320,70,70);
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    box10.display();
     bird.display();
     platform.display();
 }
